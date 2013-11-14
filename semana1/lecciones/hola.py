b5d97b7775ae70792c800dbe8342f5865681ecb7
tavooca <tavooca@gmail.com>
2013-11-14 17:17:12 -0600

    ds

--- semana1/lecciones/hola.py
+++ semana1/lecciones/hola.py
@@ -10,15 +10,16 @@ def index():
     connection = pymongo.MongoClient('localhost', 27017)
 
     # adjuntamos la base de datos test 
-	db = connection.test
+    db = connection.test
 
-	# obtener el identificador para la recolección de nombres 
-    name = db.names
+# obtener el identificador para la recoleccion de nombres 
+
+    name = db.users
 
     # encontrar un solo documento
     item = name.find_one()
 
-return '<b>Hola %s!</b>' % item['name']
+    return "<b>Hola %s </b>" % item['name']
 
 
 bottle.run(host='localhost', port=8082)
